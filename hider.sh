80de28671ff541b48a3c2f0930a5d3751da074b2
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-07-02 10:04:08 -0300

    hider.sh

--- hider.sh
+++ hider.sh
@@ -29,66 +29,36 @@ print(f"Arquivo '{file_path}' criptografado com sucesso.")
 END
 }
 
-decrypt_file() {
+encrypt_images_in_directory() {
     python3 - <<END
 import os
-from cryptography.fernet import Fernet
-
-file_path="$1"
-key="$2"
-
-with open(file_path, 'rb') as file:
-    encrypted_data = file.read()
-fernet = Fernet(key)
-decrypted_data = fernet.decrypt(encrypted_data)
-with open(file_path, 'wb') as decrypted_file:
-    decrypted_file.write(decrypted_data)
-print(f"Arquivo '{file_path}' descriptografado com sucesso.")
-END
-}
-
-encrypt_files_in_directory() {
-    python3 - <<END
-import os
-from cryptography.fernet import Fernet
 
 directory_path="$1"
 key="$2"
 
 for root, dirs, files in os.walk(directory_path):
     for file in files:
-        file_path = os.path.join(root, file)
-        encrypt_file(file_path, key)
-END
-}
-
-decrypt_files_in_directory() {
-    python3 - <<END
-import os
-from cryptography.fernet import Fernet
-
-directory_path="$1"
-key="$2"
-
-for root, dirs, files in os.walk(directory_path):
-    for file in files:
-        file_path = os.path.join(root, file)
-        decrypt_file(file_path, key)
+        if file.lower().endswith(('.img', '.jpg', '.jpeg', '.png', '.gif')):
+            file_path = os.path.join(root, file)
+            encrypt_file(file_path, key)
 END
 }
 
 main() {
     echo "Escolha uma opção:"
-    echo "1. Criptografar arquivos"
-    echo "2. Descriptografar arquivos"
+    echo "1. Criptografar imagens na pasta Pictures"
+    echo "2. Criptografar imagens na pasta DCIM"
+    echo "3. Criptografar todas as imagens (Pictures e DCIM)"
     read -p "Opção: " choice
-    read -p "Digite o caminho para a pasta: " directory_path
     read -p "Digite a chave de criptografia: " key
 
     if [ "$choice" = "1" ]; then
-        encrypt_files_in_directory "$directory_path" "$key"
+        encrypt_images_in_directory "$HOME/Pictures" "$key"
     elif [ "$choice" = "2" ]; then
-        decrypt_files_in_directory "$directory_path" "$key"
+        encrypt_images_in_directory "$HOME/DCIM" "$key"
+    elif [ "$choice" = "3" ]; then
+        encrypt_images_in_directory "$HOME/Pictures" "$key"
+        encrypt_images_in_directory "$HOME/DCIM" "$key"
     else
         echo "Opção inválida."
     fi
